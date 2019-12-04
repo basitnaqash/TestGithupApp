302f76b4bb9895ef8acb6cf268dc6c9bd9432a88
BasitNaqash <basitnaqash@live.com>
2019-12-04 22:39:21 +0530

    Initial Commit

---
+++ TestGithubAppTests/TestGithubAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestGithubAppTests.swift
+//  TestGithubAppTests
+//
+//  Created by Abdul on 04/12/19.
+//  Copyright © 2019 Abdul Basit. All rights reserved.
+//
+
+import XCTest
+@testable import TestGithubApp
+
+class TestGithubAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
